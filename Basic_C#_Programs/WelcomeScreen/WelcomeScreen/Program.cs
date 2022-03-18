6ca08890eba21b07ce3421884f813c7200480217
Brandon Oh <brandonoh2938@gmail.com>
2022-03-18 18:08:22 -0400

    Created wecome screen that utilizes escape sequences.

---
+++ Basic_C#_Programs/WelcomeScreen/WelcomeScreen/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WelcomeScreen
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to ACME Accounting Systems" + "\n" + "Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
