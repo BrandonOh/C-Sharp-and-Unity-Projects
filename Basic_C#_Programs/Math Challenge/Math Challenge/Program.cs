b2740872689314f64b594796de9143801d274217
Brandon Oh <brandonoh2938@gmail.com>
2022-03-18 19:32:41 -0400

    Simple math example console app

---
+++ Basic_C#_Programs/Math Challenge/Math Challenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Math_Challenge
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Please input a whole number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please input another whole number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = num1 + num2;
+            Console.WriteLine("Those numbers added to each other is: " + num3);
+            num3 = num1 - num2;
+            Console.WriteLine("Those numbers subtracted to each other is: " + num3);
+            num3 = num1 * num2;
+            Console.WriteLine("Those numbers multiplied by each other is: " + num3);
+            num3 = num1 / num2;
+            Console.WriteLine("Those numbers divided by each other is: " + num3);
+            Console.ReadLine();
+        }
+    }
+}
