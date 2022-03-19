220b6ef0711f03aa763faba8769693500d2ac086
Brandon Oh <brandonoh2938@gmail.com>
2022-03-18 22:25:50 -0400

    Approval program for car insurance

---
+++ Basic_C#_Programs/Approval Program/Approval Program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Approval_Program
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("What is your age? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Have you ever had a DUI? ");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+            Console.Write("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            bool qualified = ((age > 15) && (DUI == false) && (tickets <= 3));
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
