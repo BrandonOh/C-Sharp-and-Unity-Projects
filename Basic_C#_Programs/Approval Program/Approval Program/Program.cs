d0d22ee1946651ac816f20f923a0df971001d692
Brandon Oh <brandonoh2938@gmail.com>
2022-03-29 15:28:27 -0400

    Added if/else statement for qualified answer

--- Basic_C#_Programs/Approval Program/Approval Program/Program.cs
+++ Basic_C#_Programs/Approval Program/Approval Program/Program.cs
@@ -6,6 +6,7 @@ namespace Approval_Program
     {
         static void Main()
         {
+            bool qualified;
             Console.Write("What is your age? ");
             int age = Convert.ToInt32(Console.ReadLine());
             Console.Write("Have you ever had a DUI? ");
@@ -13,7 +14,14 @@ namespace Approval_Program
             Console.Write("How many speeding tickets do you have?");
             int tickets = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Qualified?");
-            bool qualified = ((age > 15) && (DUI == false) && (tickets <= 3));
+            if ((age > 15) && (DUI == false) && (tickets <= 3))
+            {
+                qualified = true;
+            }
+            else
+            {
+                qualified = false;
+            }
             Console.WriteLine(qualified);
             Console.ReadLine();
         }
