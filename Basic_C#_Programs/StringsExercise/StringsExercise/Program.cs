5f6e3ae15d3eb402196193e7bb71a23e9a92e17c
Brandon Oh <brandonoh2938@gmail.com>
2022-03-29 18:41:55 -0400

    String exercise for StringBuilder and concatenating.

---
+++ Basic_C#_Programs/StringsExercise/StringsExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace StringsExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            string firstSent;
+
+            Console.WriteLine("Hello, Whats your name?");
+            string name = Console.ReadLine();
+            firstSent = "Your name is " + name.ToUpper();
+            Console.WriteLine("How old are you?");
+            string age = Console.ReadLine();
+            firstSent += " your " + age + " years old ";
+            Console.WriteLine("What is your favorite hobby?");
+            string hobby = Console.ReadLine();
+            firstSent += "and your favorite hobby is " + hobby + ".";
+            Console.WriteLine(firstSent + "\n");
+            sb.Append("According to all known laws of aviation, there is no way a bee should be able to fly.\n");
+            sb.Append("It's wings are too small to get its fat little body off the ground.\n");
+            sb.Append("The bee, of course, flies anyway, because bees don't care what humans think is impossible.\n");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
