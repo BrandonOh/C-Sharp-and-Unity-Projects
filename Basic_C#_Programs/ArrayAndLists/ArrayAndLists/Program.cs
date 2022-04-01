f0077980443cdafb41178d5e949f4f4156fa05d5
Brandon Oh <brandonoh2938@gmail.com>
2022-04-01 18:44:01 -0400

    Array and List examples and Exercise

---
+++ Basic_C#_Programs/ArrayAndLists/ArrayAndLists/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayAndLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*
+            int[] numArray = new int[5];
+            numArray[0] = 5;
+            numArray[1] = 2;
+            numArray[2] = 10;
+            numArray[3] = 200;
+            numArray[4] = 5000;
+
+            int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+
+            int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+            numArray2[5] = 650;
+
+            Console.WriteLine(numArray2[5]);
+            Console.ReadLine();
+            */
+
+            List<string> intList = new List<string>();
+            intList.Add("Hello");
+            intList.Add("World");
+            intList.Remove("World");
+
+            Console.WriteLine(intList[0]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/ArrayExercise/ArrayExercise/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] arrayStrings = new string[5] { "Bob", "Rick", "Abby", "John", "Jane" };
+            int[] arrayInt = new int[5] { 16, 24, 13, 28, 31 };
+
+            Console.WriteLine("Pick a number between 1-5 to find the name and age of a person.");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num > 5 || num < 0)
+            {
+                Console.WriteLine("That number is not within the range.");
+                Console.ReadLine();
+            }
+            else
+            {
+                num -= 1;
+                Console.WriteLine(arrayStrings[num] + " " + arrayInt[num]);
+                Console.ReadLine();
+            }
+
+            List<string> occupation = new List<string>();
+            occupation.Add("Police");
+            occupation.Add("Teacher");
+            occupation.Add("Banker");
+            occupation.Add("Manager");
+            occupation.Add("Cook");
+
+            Console.WriteLine("Pick a number between 1-5 to find occupation name.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > 5 || num1 < 0)
+            {
+                Console.WriteLine("That number is not within the range.");
+                Console.ReadLine();
+            }
+            else
+            {
+                num1 -= 1;
+                Console.WriteLine(occupation[num1]);
+                Console.ReadLine();
+            }
+        }
+    }
+}
