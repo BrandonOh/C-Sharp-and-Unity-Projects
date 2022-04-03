84e91917fbd353050525c639e49ac36dcd59add6
Brandon Oh <brandonoh2938@gmail.com>
2022-04-02 22:18:55 -0400

    Main Method program written.

---
+++ Basic_C#_Programs/MainMethodExercise/MainMethodExercise/Operation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodExercise
+{
+    class Operation
+    {
+        public static int multBy100 (int num1)
+        {
+            int c = num1 * 100;
+            return c;
+        }
+
+        public static int multBy100(decimal num1)
+        {
+            decimal c = num1 * 100;
+            return Convert.ToInt32(c);
+        }
+
+        public static int multBy100(string num1)
+        {
+            int c = 0;
+            try
+            {
+                c = Convert.ToInt32(num1) * 100;
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine(fe.Message);
+            }
+            
+            return c;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MainMethodExercise/MainMethodExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Operation.multBy100(4));
+            Console.WriteLine(Operation.multBy100(20.5M));
+            Console.WriteLine(Operation.multBy100("144"));
+            Console.ReadLine();
+        }
+    }
+}
