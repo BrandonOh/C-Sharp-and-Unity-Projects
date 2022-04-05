8099131b3caaffd064895b9447fd90c9e63c830d
Brandon Oh <brandonoh2938@gmail.com>
2022-04-04 21:22:52 -0400

    Inheritence Exercise.

--- Basic_C#_Programs/BlackJack/BlackJack/Program.cs
+++ Basic_C#_Programs/BlackJack/BlackJack/Program.cs
@@ -10,20 +10,22 @@ namespace BlackJack
     {
         static void Main(string[] args)
         {
-            Game game = new Game();
-            game.Dealer = "Brandon";
-            game.Name = "BlackJack";
+            BlackJackGame game = new BlackJackGame();
+            game.Players = new List<string>() { "Brandon", "Bob", "Samson"};
+            game.ListPlayer();
+            game.Play();
+            Console.ReadLine();
 
             Deck deck = new Deck();
             deck.Shuffle(3);
 
-            foreach(Card card in deck.Cards)
+            /*foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.face + " of " + card.suit);
             }
 
             Console.WriteLine(deck.Cards.Count);
-            Console.ReadLine();
+            Console.ReadLine();*/
         }
 
         /*public static Deck Shuffle(Deck deck, int times)
---
+++ Basic_C#_Programs/MethodAndObjectExercise/MethodAndObjectExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAndObjectExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.FirstName = "Sample";
+            emp.LastName = "Student";
+            emp.SayName();
+            Console.ReadLine();
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+
+    public class Employee : Person
+    {
+        int id;
+    }
+}
