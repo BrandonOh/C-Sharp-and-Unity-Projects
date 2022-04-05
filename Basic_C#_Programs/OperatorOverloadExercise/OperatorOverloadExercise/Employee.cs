d123340b46baff39fe37977f2a9c17f3fad19ed7
Brandon Oh <brandonoh2938@gmail.com>
2022-04-05 18:49:27 -0400

    template of overload exercise.

---
+++ Basic_C#_Programs/OperatorOverloadExercise/OperatorOverloadExercise/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadExercise
+{
+    class Employee : Person
+    {
+        public static Person operator== (Person pe, Person pe2)
+        {
+
+        }
+
+        public static Person operator!= (Person pe, Person pe2)
+        {
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/OperatorOverloadExercise/OperatorOverloadExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee em = new Employee();
+            Person pe = new Person();
+
+            pe.id = "1";
+            pe.id2 = "2";
+        }
+    }
+}
