53aac94e5ea0c5249c98d33e198bd5dfd78996cd
Brandon Oh <brandonoh2938@gmail.com>
2022-04-05 21:58:43 -0400

    Parsing Enums exercise.

---
+++ Basic_C#_Programs/ParsingEnumExercise/ParsingEnumExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            Week week = new Week();
+
+            Console.Write("Please enter the current day of the week: ");
+            string day = Console.ReadLine();
+            try
+            {
+                week.days = (Days)Enum.Parse(typeof(Days), day);
+                Console.WriteLine(week.days);
+            }
+            catch
+            {
+                Console.Write("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/ParsingEnumExercise/ParsingEnumExercise/Week.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumExercise
+{
+    public class Week
+    {
+        public Days days { get; set; }
+    }
+    public enum Days
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
