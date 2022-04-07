24bb5b623099012caa833c675ec91f82bdbb7f95
Brandon Oh <brandonoh2938@gmail.com>
2022-04-06 21:25:22 -0400

    DateTime exercise.

---
+++ Basic_C#_Programs/DateTimeExercise/DateTimeExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime);
+
+            Console.Write("\nPlease provide a number: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            dateTime = dateTime.AddHours(hours);
+            Console.WriteLine(dateTime);
+
+            Console.ReadLine();
+        }
+    }
+}
